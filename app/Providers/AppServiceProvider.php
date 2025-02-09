4c1e34f8ee556f7255bc692aa868118840865dd2
Charles Chadwick <charles@mutuallyassured.com>
2025-02-08 17:15:52 -0800

    Adding in the activity log

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\User;
+use App\Observers\ModelObserver;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        User::observe([
+            ModelObserver::class
+        ]);
     }
 }
