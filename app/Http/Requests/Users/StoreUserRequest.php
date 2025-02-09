d6e1792215a9a3a11477575ea5d2772683f30337
Charles Chadwick <charles@mutuallyassured.com>
2025-02-08 16:41:12 -0800

    Added a basic controller and some user requests.

---
+++ app/Http/Requests/Users/StoreUserRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests\Users;
+
+use App\Enums\UserRole;
+use App\Enums\UserStatus;
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class StoreUserRequest extends FormRequest {
+
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array {
+        return [
+            'role'       => ['required', 'string', Rule::in(UserRole::cases())],
+            'status'     => ['required', 'string', Rule::in(UserStatus::cases())],
+            'first_name' => ['required', 'string', 'max:255'],
+            'last_name'  => ['required', 'string', 'max:255'],
+            'email'      => ['required', 'string', 'email', 'max:255'],
+            'password'   => ['required', 'string', 'min:8', 'confirmed'],
+        ];
+    }
+}
