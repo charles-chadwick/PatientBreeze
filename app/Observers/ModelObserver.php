7215db22da22228f18178e2b84e1ca5ebef00616
Charles Chadwick <charles@mutuallyassured.com>
2025-02-08 17:13:50 -0800

    Adding in the activity log

---
+++ app/Observers/ModelObserver.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * The model that watched the database tables for changes, and logs appropriately
+ *
+ * @TODO: Change the user in ->causedBy() to reflect the auth user
+ */
+class ModelObserver {
+    public function creating(Model $model): Model {
+        $model->setAttribute('created_by', 1);
+        return $model;
+    }
+
+    public function created(Model $model): void {
+
+        activity('db-operations')
+            ->performedOn($model)
+            ->causedBy(User::find(1))
+            ->log('created');
+    }
+
+    public function updating(Model $model): Model {
+        $model->setAttribute('updated_by', 1);
+        return $model;
+    }
+
+    public function updated(Model $model): void {
+        activity('db-operations')
+            ->performedOn($model)
+            ->causedBy(User::find(1))
+            ->withProperty('old', $model->getOriginal())
+            ->log('updated');
+    }
+
+    public function deleting(Model $model): Model {
+        $model->setAttribute('deleted_by', 1);
+        return $model;
+    }
+
+    public function deleted(Model $model): void {
+
+        activity('db-operations')
+            ->performedOn($model)
+            ->causedBy(User::find(1))
+            ->log('deleted');
+    }
+}
