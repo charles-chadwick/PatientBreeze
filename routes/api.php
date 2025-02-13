01993967b499cd4cfca2b4a7a1efa97fe6624e3b
Charles Chadwick <charles@mutuallyassured.com>
2025-02-13 03:54:02 -0800

    Cleaning up API

--- routes/api.php
+++ routes/api.php
@@ -5,14 +5,6 @@ use App\Enums\DiscussionType;
 use App\Http\Controllers\DiscussionController;
 use Illuminate\Support\Facades\Route;
 
-//Route::prefix('discussions')
-//    ->group(function () {
-//        Route::get('/{type?}', [DiscussionController::class, 'index'])
-//            ->name('discussions.index');
-//        Route::post('/', [DiscussionController::class, 'store'])
-//            ->name('discussions.store');
-//
-//    });
 Route::resource('discussions', DiscussionController::class);
 Route::prefix('documents')->group(function () {
     Route::get('/', [DocumentController::class, 'index']);
